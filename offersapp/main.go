4c0921f9fdf3a625b6c9c315f9ada710254657c2
Anuj Thakur <40914997+anujkumarthakur@users.noreply.github.com>
2020-09-16 16:36:55 +0530

    Add files via upload

---
+++ offersapp/main.go
@@ -0,0 +1,71 @@
+package main
+
+import (
+	"fmt"
+	"net/http"
+	"offersapp/models"
+	"offersapp/routes"
+	"strings"
+
+	"github.com/gin-gonic/gin"
+	"github.com/jackc/pgx/v4"
+	"golang.org/x/net/context"
+)
+
+func main() {
+
+	conn, err := connectDB()
+	if err != nil {
+		return
+	}
+
+	router := gin.Default()
+
+	router.Use(dbMiddleware(*conn))
+
+	usersGroup := router.Group("users")
+	{
+		usersGroup.POST("register", routes.UsersRegister)
+		usersGroup.POST("login", routes.UsersLogin)
+	}
+	router.Run(":3000")
+}
+
+func connectDB() (c *pgx.Conn, err error) {
+	conn, err := pgx.Connect(context.Background(), "postgresql://postgres:postgres@localhost:5432/offerapp")
+	if err != nil || conn == nil {
+		fmt.Println("Error connecting to DB")
+		fmt.Println(err.Error())
+	}
+	_ = conn.Ping(context.Background())
+	return conn, err
+}
+
+func dbMiddleware(conn pgx.Conn) gin.HandlerFunc {
+	return func(c *gin.Context) {
+		c.Set("db", conn)
+		c.Next()
+	}
+}
+
+func authMiddleWare() gin.HandlerFunc {
+	return func(c *gin.Context) {
+		bearer := c.Request.Header.Get("Authorization")
+		split := strings.Split(bearer, "Bearer ")
+		if len(split) < 2 {
+			c.JSON(http.StatusUnauthorized, gin.H{"error": "Not authenticated."})
+			c.Abort()
+			return
+		}
+		token := split[1]
+		//fmt.Printf("Bearer (%v) \n", token)
+		isValid, userID := models.IsTokenValid(token)
+		if isValid == false {
+			c.JSON(http.StatusUnauthorized, gin.H{"error": "Not authenticated."})
+			c.Abort()
+		} else {
+			c.Set("user_id", userID)
+			c.Next()
+		}
+	}
+}
